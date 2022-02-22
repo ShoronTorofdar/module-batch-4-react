bbe4c91b23fbb0afc07326eae6d4c149841af6a1
ShoronTorofdar <86644344+ShoronTorofdar@users.noreply.github.com>
2022-02-22 14:34:54 +0600

    react start module  46-start

---
+++ 46-react-rest-countries/src/App.js
@@ -0,0 +1,84 @@
+import './App.css';
+import Countries from './components/Countries/Countries';
+
+
+function App() {
+  return (
+  <div className='App'>
+   <Countries/> 
+  </div>
+  );
+}
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function Countries(){
+// const [countries, setCountries] = useState([]);
+// useEffect(() => {
+//   fetch('https://restcountries.com/v2/all')
+//   .then(response => response.json())
+//   .then(data => setCountries(data))
+// }, []);
+
+
+//   return(
+//     <div className='text'>
+//       <h1>Countries</h1>
+//       <h2>Traveling around the World</h2>
+//      <h3>Countries Available: {countries.length}</h3>
+//      {
+//        countries.map(country => <Country name={country.name} capital ={country.capital} population = {country.population}></Country>)
+//      }
+//     </div>
+//   )
+// }
+
+// function Country (props){
+//   return(
+//     <div className='text country'>
+//       <h1>Country</h1>
+//       <h2>Name: {props.name}</h2>
+//       <h2>Capital: {props.capital}</h2>
+//       <h2>Population: {props.population}</h2>
+//     </div>
+//   )
+// }
---
+++ 46-react-rest-countries/src/components/Countries/Countries.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Countries = () => {
+  return (
+    <div>
+      <h1>Allah is great</h1>
+    </div>
+  );
+};
+
+export default Countries;
